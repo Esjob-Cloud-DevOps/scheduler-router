484d8145d172459ebcdfa8f66e29438b24c39f1b
gaohongtao <gaohongtao@dangdang.com>
2017-06-05 10:29:43 +0800

    support find app info

--- src/github.com/Esjob-Cloud-DevOps/scheduler-router/main.go
+++ src/github.com/Esjob-Cloud-DevOps/scheduler-router/main.go
@@ -56,6 +56,36 @@ func main() {
 			http.Redirect(w, r, url.String(), 307)
 		})
 	}
+	mux.HandleFunc("/default/api/operate/sandbox", func(w http.ResponseWriter, r *http.Request) {
+		appName := r.URL.Query().Get("appName")
+		http.Get("")
+		for _, v := range proxyMaps {
+			server, err := getAvailableServer(v)
+			if err != nil {
+				w.WriteHeader(404)
+				fmt.Fprintln(w, err)
+				log.Printf("Error: %v", err)
+				return
+			}
+			checkUrl := "http://" + server + "/api/app/" + appName
+			resp, err := http.Get(checkUrl)
+			if err != nil {
+				w.WriteHeader(500)
+				fmt.Fprintln(w, err)
+				log.Printf("Get %s: %v", checkUrl, err)
+				return
+			}
+			if resp.StatusCode == http.StatusOK {
+				targetUrl := "http://" + server + "/api/operate/sandbox?appName=" + appName
+				log.Printf("%s is redirected to url: %s", "/default/api/operate/sandbox", targetUrl)
+				http.Redirect(w, r, targetUrl, 307)
+				return
+			}
+		}
+		w.WriteHeader(404)
+		fmt.Fprintln(w, "Can find app")
+		log.Printf("Can find app %s", appName)
+	})
 
 	http.ListenAndServe(*addr, mux)
 }
