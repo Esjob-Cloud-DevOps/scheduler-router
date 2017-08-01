f97da8a3a7c8619395b10b215662283f88d83361
gaohongtao <gaohongtao@dangdang.com>
2017-08-01 16:55:57 +0800

    Refactor to find app

--- src/github.com/Esjob-Cloud-DevOps/scheduler-router/main.go
+++ src/github.com/Esjob-Cloud-DevOps/scheduler-router/main.go
@@ -1,73 +1,32 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"strings"
-	"time"
 )
 
 var (
 	addr = flag.String("listen", ":8899", "The address of listen on")
-	conf = flag.String("conf", "", "The config string of JSON type")
+	roles = flag.String("roles", "", "The roles of frameworks")
 )
 
-func getAvailableServer(servers []string) (string, error) {
-	for _, s := range servers {
-		if _, err := net.DialTimeout("tcp", s, time.Second*2); err == nil {
-			return s, nil
-		}
-	}
-	return "", errors.New(fmt.Sprintf("All servers %v are unavailable", servers))
-}
 func main() {
 	flag.Parse()
-	if len(*conf) < 1 {
-		log.Fatal("Lack 'conf' param")
+	if len(*roles) < 1 {
+		log.Fatal("Lack 'roles' param")
 	}
 
-	log.Printf("conf str: %s", *conf)
-	proxyMaps := make(map[string][]string)
-	if err := json.Unmarshal([]byte(*conf), &proxyMaps); err != nil {
-		log.Panic(err)
-	}
-	log.Printf("conf: %v", proxyMaps)
+	log.Printf("roles str: %s", *roles)
+
 	mux := http.NewServeMux()
-	for k, v := range proxyMaps {
-		servers := v
-		mux.HandleFunc("/"+k+"/", func(w http.ResponseWriter, r *http.Request) {
-			server, err := getAvailableServer(servers)
-			if err != nil {
-				w.WriteHeader(400)
-				fmt.Fprintln(w, err)
-				log.Printf("Error: %v", err)
-				return
-			}
-			url := r.URL
-			oldUrl := url.String()
-			url.Host = server
-			url.Path = strings.Join(strings.Split(url.Path, "/")[2:], "/")
-			log.Printf("%s is redirected to url: %s", oldUrl, url.String())
-			http.Redirect(w, r, url.String(), 307)
-		})
-	}
 	mux.HandleFunc("/default/api/operate/sandbox", func(w http.ResponseWriter, r *http.Request) {
 		appName := r.URL.Query().Get("appName")
-		http.Get("")
-		for _, v := range proxyMaps {
-			server, err := getAvailableServer(v)
-			if err != nil {
-				w.WriteHeader(404)
-				fmt.Fprintln(w, err)
-				log.Printf("Error: %v", err)
-				return
-			}
-			checkUrl := "http://" + server + "/api/app/" + appName
+		for _, role := range strings.Split(*roles, ",") {
+
+			checkUrl := "http://jobcloud.api:8899/" + role + "/api/app/" + appName
 			resp, err := http.Get(checkUrl)
 			if err != nil {
 				w.WriteHeader(500)
@@ -76,7 +35,7 @@ func main() {
 				return
 			}
 			if resp.StatusCode == http.StatusOK {
-				targetUrl := "http://" + server + "/api/operate/sandbox?appName=" + appName
+				targetUrl := "http://jobcloud.api:8899/" + role + "/api/operate/sandbox?appName=" + appName
 				log.Printf("%s is redirected to url: %s", "/default/api/operate/sandbox", targetUrl)
 				http.Redirect(w, r, targetUrl, 307)
 				return
